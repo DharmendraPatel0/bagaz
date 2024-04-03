56aa98ee49a624f7a02664b518e1ed5005a1a5b8
Dharmendra-Patel-Pfgc <dharmendra.patel@pfgc.com>
2024-04-03 13:44:45 +0530

    changes added

--- src/app/sidebar/page.jsx
+++ src/app/sidebar/page.jsx
@@ -5,29 +5,28 @@ import Work from "../components/work/page";
 import About from "../components/about/page";
 import Contact from "../components/contact/page";
 function Sidebar() {
-  const [isopen, setisopen] = useState(false);
-  const [istrue, setistrue] = useState(false);
-  const [ispressed, setispressed] = useState(false);
+  const [openSidebarObj, setOpenSidebarObj] = useState({
+    work: false,
+    about: false,
+    contact: false,
+  });
+
+  const handleOpenParticularSidebar = (name) => {
+    setOpenSidebarObj({ ...openSidebarObj, [name]: !openSidebarObj?.[name] });
+  };
+
   return (
     <div className="container">
-      <button onClick={() => setisopen((prev) => !prev)}>Work</button>
-      {
-        isopen && (
-          <Work/>
-        )
-      }
-      <button onClick={() => setistrue((prev) => !prev)}>About</button>
-      {
-        istrue && (
-          <About/>
-        )
-      }
-      <button onClick={() => setispressed((prev) => !prev)}>Contact</button>
-      {
-        ispressed && (
-          <Contact/>
-        )
-      }
+      <button onClick={() => handleOpenParticularSidebar("work")}>Work</button>
+      {openSidebarObj?.work && <Work />}
+      <button onClick={() => handleOpenParticularSidebar("about")}>
+        About
+      </button>
+      {openSidebarObj?.about && <About />}
+      <button onClick={() => handleOpenParticularSidebar("contact")}>
+        Contact
+      </button>
+      {openSidebarObj?.contact && <Contact />}
     </div>
   );
 }
